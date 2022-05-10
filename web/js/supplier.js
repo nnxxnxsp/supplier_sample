49c6bc11496aab793a5da9567856495dc9ee7f06
nnxxnx <nnxxnx@qq.com>
2022-05-10 22:57:03 +0800

    Improve js codes.

--- web/js/supplier.js
+++ web/js/supplier.js
@@ -1,79 +1,88 @@
 $(document).ready(function () {
-    $('.supplier-index').on('click', '.select-on-check-all', function (e) {
-        let messageBox = $('#supplier-message-box');
-        if ($(this).prop('checked')) {
-            let selectedCount = $("input[name='selection[]']").length;
+    $('.supplier-index')
+        /* Bind select all check box check/uncheck action */
+        .on('click', '.select-on-check-all', function (e) {
+            let messageBox = $('#supplier-message-box');
+            if ($(this).prop('checked')) {
+                let selectedCount = $("input[name='selection[]']").length;
+                let message = `All ${selectedCount} records on this page have been selected. <a href="javascript:" ` +
+                    `class="select_all_records">Select all records match this search.</a>`;
+                messageBox.removeClass('alert-primary')
+                    .addClass('alert-danger')
+                    .css('display', 'block')
+                    .html(message);
+            } else {
+                $('input[name=select_all_records]').val(0);
+                messageBox.css('display', 'none')
+                    .html('');
+            }
+        })
+        /* Bind select all <a> tag click action */
+        .on('click', '.select_all_records', function () {
+            let messageBox = $('#supplier-message-box');
+            let message = `All records in this search have been selected. <a href="javascript:" ` +
+                `class="clear_select_all_records">Clear selection.</a>`;
+            messageBox.removeClass('alert-danger')
+                .addClass('alert-primary')
+                .css('display', 'block')
+                .html(message);
+            $('input[name=select_all_records]').val(1);
+        })
+        /* Bind deselect all <a> tag click action */
+        .on('click', '.clear_select_all_records', function () {
+            let messageBox = $('#supplier-message-box');
+            let selectedCount = $("#w0").yiiGridView('getSelectedRows').length;
             let message = `All ${selectedCount} records on this page have been selected. <a href="javascript:" ` +
                 `class="select_all_records">Select all records match this search.</a>`;
             messageBox.removeClass('alert-primary')
                 .addClass('alert-danger')
                 .css('display', 'block')
                 .html(message);
-        } else {
-            messageBox.css('display', 'none')
-                .html('');
-        }
-    }).on('click', '.select_all_records', function () {
-        let messageBox = $('#supplier-message-box');
-        let message = `All records in this search have been selected. <a href="javascript:" ` +
-            `class="clear_select_all_records">Clear selection.</a>`;
-        messageBox.removeClass('alert-danger')
-            .addClass('alert-primary')
-            .css('display', 'block')
-            .html(message);
-        $('input[name=select_all_records]').val(1);
-    }).on('click', '.clear_select_all_records', function () {
-        let messageBox = $('#supplier-message-box');
-        let selectedCount = $("#w0").yiiGridView('getSelectedRows').length;
-        let message = `All ${selectedCount} records on this page have been selected. <a href="javascript:" ` +
-            `class="select_all_records">Select all records match this search.</a>`;
-        messageBox.removeClass('alert-primary')
-            .addClass('alert-danger')
-            .css('display', 'block')
-            .html(message);
-        $('input[name=select_all_records]').val(0);
-    }).on('click', '.supplier-export', function () {
-        let selectAllRecord = $('input[name=select_all_records]').val();
-        let searchParams = {};
-        if (selectAllRecord === '1') {
-            searchParams.id_compare_op = $("select[name='SupplierSearch[id_compare_op]']").val();
-            searchParams.id_compare_number = $("input[name='SupplierSearch[id_compare_number]']").val();
-            searchParams.name = $("input[name='SupplierSearch[name]']").val();
-            searchParams.code = $("input[name='SupplierSearch[code]']").val();
-            searchParams.t_status = $("select[name='SupplierSearch[t_status]']").val();
-        } else {
-            let selectedIds = $("#w0").yiiGridView('getSelectedRows');
-            if (selectedIds.length === 0) {
-                alert('Please select some records first.');
-                return;
+            $('input[name=select_all_records]').val(0);
+        })
+        /* Bind export button action */
+        .on('click', '.supplier-export', function () {
+            let selectAllRecord = $('input[name=select_all_records]').val();
+            let searchParams = {};
+            if (selectAllRecord === '1') {
+                searchParams.id_compare_op = $("select[name='SupplierSearch[id_compare_op]']").val();
+                searchParams.id_compare_number = $("input[name='SupplierSearch[id_compare_number]']").val();
+                searchParams.name = $("input[name='SupplierSearch[name]']").val();
+                searchParams.code = $("input[name='SupplierSearch[code]']").val();
+                searchParams.t_status = $("select[name='SupplierSearch[t_status]']").val();
+            } else {
+                let selectedIds = $("#w0").yiiGridView('getSelectedRows');
+                if (selectedIds.length === 0) {
+                    alert('Please select some records first.');
+                    return;
+                }
+                searchParams.selected_ids = selectedIds.join(',');
             }
-            searchParams.selected_ids = selectedIds.join(',');
-        }
 
-        $.ajax({
-            type: 'POST',
-            data: searchParams,
-            url: '/index.php?r=supplier/export-to-csv',
-            error: function (XMLHttpRequest, textStatus, errorThrown) {
-                alert('Download failed.');
-            },
-            success: function (data, status, xhr) {
-                let blob = new Blob([data]);
-                let link = document.createElement('a');
-                link.href = window.URL.createObjectURL(blob);
-                let filename = '';
-                let disposition = xhr.getResponseHeader('Content-Disposition');
-                if (disposition && disposition.indexOf('attachment') !== -1) {
-                    let filenameRegex = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/;
-                    let matches = filenameRegex.exec(disposition);
-                    if (matches != null && matches[1]) filename = matches[1].replace(/['"]/g, '');
+            $.ajax({
+                type: 'POST',
+                data: searchParams,
+                url: '/index.php?r=supplier/export-to-csv',
+                error: function (XMLHttpRequest, textStatus, errorThrown) {
+                    alert('Download failed.');
+                },
+                success: function (data, status, xhr) {
+                    let blob = new Blob([data]);
+                    let link = document.createElement('a');
+                    link.href = window.URL.createObjectURL(blob);
+                    let filename = '';
+                    let disposition = xhr.getResponseHeader('Content-Disposition');
+                    if (disposition && disposition.indexOf('attachment') !== -1) {
+                        let filenameRegex = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/;
+                        let matches = filenameRegex.exec(disposition);
+                        if (matches != null && matches[1]) filename = matches[1].replace(/['"]/g, '');
+                    }
+                    if (filename === '') {
+                        filename = 'SupplierExport.csv';
+                    }
+                    link.download = filename;
+                    link.click();
                 }
-                if (filename === '') {
-                    filename = 'SupplierExport.csv';
-                }
-                link.download = filename;
-                link.click();
-            }
+            });
         });
-    });
 })
